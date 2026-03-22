2e926b5b2cd21dc8ac58ff62ab899bbe287678d8
jucami90 <jucami90@gmail.com>
2026-03-22 17:36:26 -0300

    add project user microservice

---
+++ src/main/java/com/healthsystem/user/entity/User.java
@@ -0,0 +1,35 @@
+package com.healthsystem.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public User() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
